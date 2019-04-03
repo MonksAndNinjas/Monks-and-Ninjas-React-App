19bbd36cd21c07d89ebf4d42dfa7b0dcf3076a72
monksandninjas <github email address>
2019-04-03 18:55:12 +0000

    created Contact.js in /src and added react component to it

---
+++ client/src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Contact extends React.Componenet {
+  render() {
+    return (
+      <h1>This is the contact page!</h1>
+    )
+  }
+}
+
+export default Contact;
