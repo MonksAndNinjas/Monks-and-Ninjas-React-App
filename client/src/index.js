a46668a7d286afd745adb5015533bebabbeb30e8
monksandninjas <github email address>
2019-04-03 18:53:56 +0000

    created About.js in /src and added react componenet to it

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Home from './Home';
 import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 const Navbar = () =>
   <div>
@@ -12,7 +12,7 @@ const Navbar = () =>
     <NavLink to="/about" exact activeStyle={{ background: 'darkblue' }}>About</NavLink>
     <NavLink to="/training" exact activeStyle={{ background: 'darkblue' }}>Training</NavLink>
     <Navlink to="/blog" exact activeStyle={{ background: 'darkblue' }}>Blog</NavLink>
-    <NavLink to="/contact" exact activeStyle={{ background: 'darkblue' }}>Contact</NavLink>  
+    <NavLink to="/contact" exact activeStyle={{ background: 'darkblue' }}>Contact</NavLink>
   </div>;
 
 ReactDOM.render((
