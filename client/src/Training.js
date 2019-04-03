1400061994e240ff369721ba329991d2c94baeb8
monksandninjas <github email address>
2019-04-03 18:57:57 +0000

    created Training.js in /src and added react component to it

---
+++ client/src/Training.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Training extends React.Component {
+  render() {
+    return (
+      <h1>This is my training page!</h1>
+    )
+  }
+}
+
+export default Training;
