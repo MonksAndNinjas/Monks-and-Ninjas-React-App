fed18b8772b036e2dd7f51b666ab3f43cf43317f
monksandninjas <github email address>
2019-04-05 17:05:42 +0000

    Added bio and philosophy to About.js in /src

--- client/src/About.js
+++ client/src/About.js
@@ -3,7 +3,26 @@ import React from 'react';
 class About extends React.Component {
   render() {
     return (
-      <h1>This is the about page!</h1>
+      <div>
+        <h1>About</h1>
+
+        <h2>Joseph Jimenez</h2>
+        <p>
+          Joseph is a  martial artist living in Boulder, Colorado.
+
+          With over 18 years experience in fitness and martial arts Joseph values listening to ones own body and life conditions in how to direct your workouts.
+          At the core of his method quality of movement comes first, forming a foundation from which to grow from.
+          By combining martial arts, dance, parkour and other various physical disciplines with concepts from meditation and learn through play I have helped people
+          of all types to improve and develop confidence in their movement. Check out my blog where you will find tips and useful resources that can help you maximize your potential.
+        </p>
+
+        <h2>Philosophy</h2>
+        <p>
+          My system of training offers a wide range of services that promotes the sense of aliveness. By emphasisng awareness of your own body and the surrounding environment
+          you can sense how your body wants to move and the limitations to overcome. Learning internal focus, can allow you to feel how food, mood and stress can affect the body's movement.
+          I have worked alongside Dr. Nadia Haddad MD, holisitic psychiatrist also trained in Chinese Medicine, to offer clients a holisitic approach to living a healthy life.
+        </p>
+      </div>
     )
   }
 }
