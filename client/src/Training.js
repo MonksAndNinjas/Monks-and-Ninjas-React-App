a576e9ea27ee4b53d45be091012d75f8672e3826
monksandninjas <github email address>
2019-04-05 17:07:45 +0000

    added type of serivce and section tag for calendar in Training.js in /src

--- client/src/Training.js
+++ client/src/Training.js
@@ -3,7 +3,18 @@ import React from 'react';
 class Training extends React.Component {
   render() {
     return (
-      <h1>This is my training page!</h1>
+      <div>
+        <h1>Training</h1>
+
+        <span>Type of Service: $Price: Duration</span>
+
+        <br/><br/>
+
+        <section>
+          Calendar should go here and switch off with description of service when button to
+          reserve is clicked.
+        </section>
+      </div>
     )
   }
 }
