8884a385b744b63483d91b47889c7374dcd96991
monksandninjas <github email address>
2019-04-09 16:39:52 +0000

    created and added to notes.txt in main directory

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import About from './About';
 import TrainingContainer from './containers/TrainingContainer';
 import BlogContainer from './containers/BlogContainer';
 import Contact from './Contact';
+
 import SocialMedia from './components/SocialMedia';
 import NavBar from './components/NavBar';
 
@@ -35,7 +36,4 @@ ReactDOM.render((
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
