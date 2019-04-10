51b6fc1b6516153014a193d3b080ff181e315d67
monksandninjas <github email address>
2019-04-10 18:26:26 +0000

    added renderPosts constant in Blog.js and render list in li tag

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -3,9 +3,16 @@ import React from 'react';
 class Blog extends React.Component {
 
   render() {
+
+    const renderPosts = this.props.blogPosts.map((post, index) => (
+      <li key={index}>{post}</li>
+    ));
+
     return (
       <div>
         <h2>All Blog Posts go here</h2>
+
+        <ul>{renderPosts}</ul>
       </div>
     )
   }
