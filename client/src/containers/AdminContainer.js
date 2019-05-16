b0053c8ddfc0d636771f627266fbba69088e6479
monksandninjas <github email address>
2019-05-16 16:02:07 +0000

    created /admin folder along with Login.js and added React component to it

--- client/src/containers/AdminContainer.js
+++ client/src/containers/AdminContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Login from '../components/Admin/Login.js';
 
 class AdminContainer extends React.Component {
+
+  state = {
+    isLoggedIn: false
+  }
 
   render() {
     return (
       <div>
+      { this.state.isLoggedIn ? null : <Login /> }
       </div>
     )
   }
