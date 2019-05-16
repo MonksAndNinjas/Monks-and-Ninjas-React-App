89d9b33c8a048820d7ef0c674f527caa2b2a4191
monksandninjas <github email address>
2019-05-16 17:15:36 +0000

    added blog and reservation state in AdminPage.js to switch between menu items

--- client/src/components/Admin/AdminPage.js
+++ client/src/components/Admin/AdminPage.js
@@ -2,17 +2,34 @@ import React from 'react';
 
 class AdminPage extends React.Component {
 
+  state = {
+    displayBlog: false,
+    displayReservations: false
+  }
+
   handleClick = event => {
     event.preventDefault();
+    const path = event.target.name
 
-    this.props.logout()
+
+    if (path === 'blog') {
+      this.setState({ displayBlog: true })
+    } else if (path === 'reservations') {
+      this.setState({ displayReservations: true })
+    } else {
+      this.props.logout()
+    }
   }
 
   render() {
     return (
       <div>
-        Admin page
-        <button onClick={event => this.handleClick(event)} >Log Out</button>
+        <h1>Admin page</h1>
+
+        <button name='blog' onClick={event => this.handleClick(event)}>Blog</button>
+        <button name='reservations' onClick={event => this.handleClick(event)}>Reservations</button>
+
+        <button onClick={event => this.handleClick(event)}>Log Out</button>
       </div>
     )
   }
