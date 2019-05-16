a16d5eb6fe0a6e1ab5e80cf5e99c379b3b77a776
monksandninjas <github email address>
2019-05-16 16:17:39 +0000

    added handleSubmit and handleChange in Login.js

--- client/src/components/Admin/Login.js
+++ client/src/components/Admin/Login.js
@@ -2,20 +2,45 @@ import React from 'react';
 
 class Login extends React.Component {
 
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange = event => {
+    event.persist()
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(event.target.username, event.target.password, event.target.value)
+    /*fetch('api/posts', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })*/
+  }
+
   render() {
     return (
       <div>
         <h1>Admin Login</h1>
         <form onSubmit={event => this.handleSubmit(event)}>
           <label>
-            Title
-            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+            Username
+            <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
           </label>
           <label>
-            Content
-            <input type="textarea" name="content" value={this.state.content} onChange={this.handleChange} />
+            Password
+            <input type="textarea" name="password" value={this.state.password} onChange={this.handleChange} />
           </label>
-          <button type="submit">Submit Post</button>
+          <button type="submit">Login</button>
         </form>
       </div>
     )
