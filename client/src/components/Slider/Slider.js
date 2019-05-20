27d56be59b97b8ae4ce0eef2f3bc4191335e863a
blackbeltdiaries <github email address>
2019-05-20 14:41:46 -0600

    created goToPrev and NextSlide functions in Slider.js

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import Slide from '../slide';
+import LeftArrow from '../left-arrow';
+import RightArrow from '../right-arrow';
 
 class Slider extends React.Component {
 
   state = {}
 
+  goToPrevSlide = () => {
+
+  }
+
+  goToNextSlide = () => {
+
+  }
+
   render() {
     return (
       <div className="slider">
+        <Slide />
+
+        <LeftArrow goToPrevSlide={this.goToPrevSlide} />
+        <RightArrow goToNextSlide={this.goToNextSlide} />
       </div>
     )
   }
