21bd22d939260e3f16ce9964abd4e2b1a5eaff4c
blackbeltdiaries <github email address>
2019-05-21 15:07:15 -0600

    added css to index.css for

--- client/src/About.js
+++ client/src/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 class About extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>About</h1>
 
         <h2>Joseph Jimenez</h2>
--- client/src/Home.js
+++ client/src/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Monks And Ninjas</h1>
         <h2>Body Awareness Meditative Martial Arts Life Performance</h2>
       </div>
