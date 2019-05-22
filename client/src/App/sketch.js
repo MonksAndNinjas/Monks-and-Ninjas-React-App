64ffaa6eaf64c6b6b9dbca92f711f7bd4dc87021
blackbeltdiaries <github email address>
2019-05-22 17:56:41 -0600

    added more triangles and scale for sketch.js

--- client/src/App/sketch.js
+++ client/src/App/sketch.js
@@ -9,13 +9,55 @@ export default function sketch (p) {
     let width = window.outerWidth;
     let height = window.outerHeight * 2;
     let stepDistance = 10;
-
+    let multiplier = (width / 10) / 140;
+// width / 10 => 1 scale; width / 5 => 2;
     p.background('white')
     p.stroke('#eeee')
 
+    p.translate(width / 10, height / 2)
+    p.scale(multiplier)
+
     p.translate(stepDistance + stepMultiplier, 0)
-    p.triangle((width / 10) + 50, height / 2, width / 10, (height / 2) + 150, (width / 10) - 50, height / 2)
-    p.ellipse(width / 10, height / 2, width / 5, width / 5)
+
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+//    p.triangle((width / 10) + 50, height / 2, width / 10, (height / 2) + 150, (width / 10) - 50, height / 2)
+
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.ellipse(0, 0, 140, 140)
+
 
     stepMultiplier = stepMultiplier + .25;
   };
