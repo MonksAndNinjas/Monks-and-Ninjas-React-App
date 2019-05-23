bf8921543db7fa943395e9ab18ce83ff2b53ce59
blackbeltdiaries <github email address>
2019-05-23 15:41:46 -0600

    added to sketch_cloud.js and imported to sketch.js

---
+++ client/src/App/sketch_cloud.js
@@ -0,0 +1,90 @@
+export default class Cloud {
+  display (p) {
+    p.fill('white')
+    p.rect(50, 90, 200, 100)
+    p.fill(173, 216, 230, 20)
+    p.rect(50, 90, 200, 100)
+    p.triangle(0, 170, -100, 170, 0, 150)
+
+    p.fill('white')
+    p.ellipse(60, 90, 60, 60)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(60, 90, 60, 60)
+
+    p.fill('white')
+    p.ellipse(145, 150, 100, 100)
+    p.fill(0, 0, 255, 10)
+    p.ellipse(145, 150, 100, 100)
+
+    p.fill('white')
+    p.ellipse(70, 150, 130, 100)
+    p.fill(0, 0, 255, 10)
+    p.ellipse(70, 150, 130, 100)
+
+    p.fill('white')
+    p.ellipse(60, 150, 130, 100)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(60, 150, 130, 100)
+
+    p.fill('white')
+    p.ellipse(170, 60, 100, 100)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(170, 60, 100, 100)
+
+    p.fill('white')
+    p.ellipse(100, 90, 80, 80)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(100, 90, 80, 80)
+
+    p.fill('white')
+    p.ellipse(230, 80, 70, 70)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(230, 80, 70, 70)
+
+    p.fill('white')
+    p.ellipse(220, 130, 130, 130)
+    p.fill(0, 0, 255, 10)
+    p.ellipse(220, 130, 130, 130)
+
+    p.fill('white')
+    p.ellipse(230, 140, 130, 130)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(230, 140, 130, 130)
+
+    p.fill('white')
+    p.ellipse(160, 170, 80, 70)
+    p.fill(0, 0, 255, 10)
+    p.ellipse(160, 170, 80, 70)
+
+
+    p.fill('white')
+    p.ellipse(160, 180, 80, 70)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(160, 180, 80, 70)
+
+    p.fill('white')
+    p.ellipse(95, 175, 60, 60)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(95, 175, 60, 60)
+
+    p.fill('white')
+    p.ellipse(0, 155, 50, 50)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(0, 155, 50, 50)
+
+    p.fill('white')
+    p.ellipse(-20, 165, 30, 30)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(-20, 165, 30, 30)
+
+    p.fill('white')
+    p.ellipse(-40, 135, 10, 10)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(-40, 135, 10, 10)
+
+    p.fill('white')
+    p.ellipse(-60, 125, 6, 6)
+    p.fill(173, 216, 230, 20)
+    p.ellipse(-60, 125, 6, 6)
+  }
+}
