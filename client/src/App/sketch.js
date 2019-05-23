c70463c1ab5eef40e2b48ab69d010f417a0931f9
blackbeltdiaries <github email address>
2019-05-22 19:18:00 -0600

    added more triangles for sun sketch in sketch.js

--- client/src/App/sketch.js
+++ client/src/App/sketch.js
@@ -10,52 +10,64 @@ export default function sketch (p) {
     let height = window.outerHeight * 2;
     let stepDistance = 10;
     let multiplier = (width / 10) / 140;
-// width / 10 => 1 scale; width / 5 => 2;
+
     p.background('white')
-    p.stroke('#eeee')
+    p.stroke(0, 0, 0, 25)
 
     p.translate(width / 10, height / 2)
     p.scale(multiplier)
 
-    p.translate(stepDistance + stepMultiplier, 0)
+  //  p.translate(stepDistance + stepMultiplier, 0)
 
+    p.fill(255, 204, 0, 25)
     p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.rotate(100)
-//    p.triangle((width / 10) + 50, height / 2, width / 10, (height / 2) + 150, (width / 10) - 50, height / 2)
-
-    p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
     p.rotate(100)
     p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
 
+    p.rotate(100)
+    p.triangle(50, 0, 0, 150, -50, 0)
+    p.triangle(0, 0, -300, 5000, 300, 5000)
+
+    p.fill('white')
     p.ellipse(0, 0, 140, 140)
 
 
