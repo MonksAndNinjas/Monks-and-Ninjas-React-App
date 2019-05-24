60898cdfa69da8ee99080bdf3542a4ac0f0c6d6a
blackbeltdiaries <github email address>
2019-05-24 11:12:50 -0600

    refactored sketch_cloud.js

--- client/src/App/sketch_cloud.js
+++ client/src/App/sketch_cloud.js
@@ -1,90 +1,51 @@
 export default class Cloud {
+
   display (p) {
+    let v = 60;
+
     p.fill('white')
     p.rect(50, 90, 200, 100)
     p.fill(173, 216, 230, 20)
     p.rect(50, 90, 200, 100)
     p.triangle(0, 170, -100, 170, 0, 150)
 
+    this.cloudBubble(p, v, v + 30, v, v)
+
+    this.cloudBubble(p, (v * 2) + 25, (v * 2) + 30, v + 40, v + 40)
+
+    this.cloudBubble(p, v + 10, (v * 2) + 30, (v * 2) + 30, v + 40)
+
+    this.cloudBubble(p, v, (v * 2) + 30, (v * 2) + 30, v + 40)
+
+    this.cloudBubble(p, (v * 2) + 50, v, v + 40, v + 40)
+
+    this.cloudBubble(p, v + 40, v + 30, v + 20, v + 20)
+
+    this.cloudBubble(p, (v * 3) + 50, v + 20, v + 10, v + 10)
+
+    this.cloudBubble(p, (v * 3) + 40, (v * 2) + 10, (v * 2) + 10, (v * 2) + 10)
+
+    this.cloudBubble(p, (v * 3) + 50, (v * 2) + 20, (v * 2) + 10, (v * 2) + 10)
+
+    this.cloudBubble(p, (v * 2) + 40, (v * 2) + 50, v + 20, v + 10)
+
+    this.cloudBubble(p, (v * 2) + 40, (v * 2), v + 20, v + 10)
+
+    this.cloudBubble(p, v + 35, (v * 2) - 5, v, v)
+
+    this.cloudBubble(p, 0, (v * 2) + 35, v - 10, v - 10)
+
+    this.cloudBubble(p, -v + 20, (v * 2) + 45, v - 30, v - 30)
+
+    this.cloudBubble(p, -v + 20, (v * 2) + 15, v - 50, v - 50)
+
+    this.cloudBubble(p, -v, (v * 2) + 5, (v / 10), (v / 10))
+  }
+
+  cloudBubble(p, x, y, w, h) {
     p.fill('white')
-    p.ellipse(60, 90, 60, 60)
+    p.ellipse(x, y, w, h)
     p.fill(173, 216, 230, 20)
-    p.ellipse(60, 90, 60, 60)
-
-    p.fill('white')
-    p.ellipse(145, 150, 100, 100)
-    p.fill(0, 0, 255, 10)
-    p.ellipse(145, 150, 100, 100)
-
-    p.fill('white')
-    p.ellipse(70, 150, 130, 100)
-    p.fill(0, 0, 255, 10)
-    p.ellipse(70, 150, 130, 100)
-
-    p.fill('white')
-    p.ellipse(60, 150, 130, 100)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(60, 150, 130, 100)
-
-    p.fill('white')
-    p.ellipse(170, 60, 100, 100)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(170, 60, 100, 100)
-
-    p.fill('white')
-    p.ellipse(100, 90, 80, 80)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(100, 90, 80, 80)
-
-    p.fill('white')
-    p.ellipse(230, 80, 70, 70)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(230, 80, 70, 70)
-
-    p.fill('white')
-    p.ellipse(220, 130, 130, 130)
-    p.fill(0, 0, 255, 10)
-    p.ellipse(220, 130, 130, 130)
-
-    p.fill('white')
-    p.ellipse(230, 140, 130, 130)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(230, 140, 130, 130)
-
-    p.fill('white')
-    p.ellipse(160, 170, 80, 70)
-    p.fill(0, 0, 255, 10)
-    p.ellipse(160, 170, 80, 70)
-
-
-    p.fill('white')
-    p.ellipse(160, 180, 80, 70)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(160, 180, 80, 70)
-
-    p.fill('white')
-    p.ellipse(95, 175, 60, 60)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(95, 175, 60, 60)
-
-    p.fill('white')
-    p.ellipse(0, 155, 50, 50)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(0, 155, 50, 50)
-
-    p.fill('white')
-    p.ellipse(-20, 165, 30, 30)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(-20, 165, 30, 30)
-
-    p.fill('white')
-    p.ellipse(-40, 135, 10, 10)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(-40, 135, 10, 10)
-
-    p.fill('white')
-    p.ellipse(-60, 125, 6, 6)
-    p.fill(173, 216, 230, 20)
-    p.ellipse(-60, 125, 6, 6)
+    p.ellipse(x, y, w, h)
   }
 }
