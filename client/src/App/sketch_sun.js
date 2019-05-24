af48bc81b6e27af52243f2318fc91a1dca2af084
blackbeltdiaries <github email address>
2019-05-24 10:38:36 -0600

    refactored sketch_sun.js

--- client/src/App/sketch_sun.js
+++ client/src/App/sketch_sun.js
@@ -1,9 +1,7 @@
 export default class Sun {
-  constructor () {
-
-  }
 
   display(p) {
+    // used for making gradient of sun
     let from = p.color(255, 204, 0, 4);
     let to = p.color(255, 250, 250, 4);
     let inter = p.lerpColor(from, to, .1)
@@ -11,126 +9,29 @@ export default class Sun {
     p.fill(inter)
     p.noStroke()
 
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-////////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-///////////////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-//////////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-/////////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-//////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-//////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-///////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-///////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-///////////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-/////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
-/////////////////
-    p.rotate(100)
-    p.triangle(50, 0, 0, 150, -50, 0)
-
-    p.push()
-    p.rotate(Math.PI/16)
-    p.triangle(50, 0, 0, 300, -50, 0)
-    p.pop()
-
-    p.triangle(0, 0, -300, 5000, 300, 5000)
+     // sun rays
+    for(var i = 0; i < 12; i++) {
+      if (i === 0) {
+        this.sunRays(p);
+      } else {
+        p.rotate(100);
+        this.sunRays(p);
+      }
+    }
 
     p.fill('white')
     p.ellipse(0, 0, 140, 140)
+  }
+
+  // sun
+  sunRays(p) {
+    p.triangle(50, 0, 0, 150, -50, 0)
+
+    p.push()
+    p.rotate(Math.PI/16)
+    p.triangle(50, 0, 0, 300, -50, 0)
+    p.pop()
+
+    p.triangle(0, 0, -300, 5000, 300, 5000)
   }
 }
